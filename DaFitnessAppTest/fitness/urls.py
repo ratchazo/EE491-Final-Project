a010568c89134b2c805d9f9a3f3a2770e6eac12a
Jacob <jacobam9@hawaii.edu>
2022-06-29 17:04:10 -1000

    attempting to add CRUD

--- DaFitnessAppTest/fitness/urls.py
+++ DaFitnessAppTest/fitness/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('calculator', views.IndexView.as_view(), name='index'),
     path('daily-calorie-intake', views.ResultsView.as_view(), name='results'),
+    path('new', views.CreateView.as_view(), name='create'),
+    path('update/<int:pk>', views.UpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.DeleteView.as_view(), name='delete'),
 ]
--- DaFitnessAppTest/fitness/views.py
+++ DaFitnessAppTest/fitness/views.py
@@ -23,4 +23,21 @@ class ResultsView(generic.edit.CreateView):
     model = fitness
     fields = ['message']
     success_url = reverse_lazy('fitness:home')
+
+class CreateView(generic.edit.CreateView):
+    template_name = 'fitness/create.html'
+    model = fitness
+    fields = ['message']
+    success_url = reverse_lazy('fitness:blogs') # more robust than hardcoding to /greetings/; directs user to index view after creating a greeting
+
+class UpdateView(generic.edit.UpdateView):
+    template_name = 'fitness/update.html'
+    model = fitness
+    fields = ['message']
+    success_url = reverse_lazy('fitness:blogs')
+
+class DeleteView(generic.edit.DeleteView):
+    template_name = 'fitness/delete.html' # override default of greetings/greeting_confirm_delete.html
+    model = fitness
+    success_url = reverse_lazy('fitness:blogs')
     
